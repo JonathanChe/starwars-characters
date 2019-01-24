693ffcb29a13cdadd5692b6c5de7ca1b9fac2c4b
Jonathan <jonathan.che@outlook.com>
2019-01-23 18:03:06 -0800

    wallpaper set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+require('babel-polyfill');
 
-const App = () => {
-  return (
-    <div>
-      Star Wars
-    </div>
-  );
-};
+class App extends Component {
+
+  async componentDidMount() {
+    const data = await fetch('https://swapi.co/api/people/')
+      .then(response => response.json())
+      .then(response => response.results);
+  }
+
+  render() {
+    return (
+      <div id="wallpaper">
+        <h1 id="title">Star Wars Characters</h1>
+      </div>
+    );
+  }
+}
 
 export default App;
